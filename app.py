db3e791b57a558fd19225187c85c0fc346c88e6a
Divyansh Bansal <bansal9divyansh@gmail.com>
2024-10-20 01:01:44 +0530

    Update app.py
    cloud hosting not possible at this moment

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ def scrape():
             options = webdriver.ChromeOptions()
             options.add_argument('--no-sandbox')
             options.add_argument("--log-level=3")
-            options.add_argument("--headless")
             mobile_emulation = {
                 "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
             options.add_experimental_option("mobileEmulation", mobile_emulation)
